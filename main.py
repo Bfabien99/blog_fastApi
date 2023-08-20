117cfeb092d6a32aed30598cd8b27a9cfa26fc7e
Brou Kouadio Stéphane Fabien <fabienbrou99@gmail.com>
2023-08-20 18:10:30 +0000

    querry parameters

--- main.py
+++ main.py
@@ -7,9 +7,19 @@ def index():
     return {"data":{"creator":"Brou Kouadio Stéphane-Fabien"}}
 
 @app.get('/blogs')
-def get_blogs():
-    return {"data":"show all blogs"}
+def get_blogs(limit: int=100):
+    return {"data":f"{limit} blogs from db"}
+
+@app.get('/blogs/unpublished')
+def get_unpublished_blogs():
+    return {"data":"show all unpublished blogs"}
 
 @app.get('/blog/{id}')
-def get_blog(id):
-    return {"data":"show blog "+id}
+def get_blog(id: int):
+    # fetch blog with id = id
+    return {"data":f"show blog {id}"}
+
+@app.get('/blog/{id}/comments')
+def get_blog_comments(id: int):
+    # fetch comments of blog with id = id
+    return {"data":{"comments":f"comments of blog {id}"}}
